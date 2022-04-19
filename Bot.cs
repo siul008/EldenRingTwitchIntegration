103848a295565501239014fddb0f7a24bc944d65
siul008 <Julien.nunespro@gmail.com>
2022-04-19 23:18:09 +0200

    ..

--- Bot.cs
+++ Bot.cs
@@ -21,6 +21,9 @@ namespace Tricherie
     public class Bot
     {
         public static TwitchClient client;
+        // readonly string changeStatsId = "custom-reward-id=f88c3bed-a091-4b9b-abd3-a27d7a684070"; // siul008
+        // readonly string damageMeId = "custom-reward-id=43d90440-3213-4ec7-9dd1-158e3a679080"; // siul008
+        //readonly string healMeId = "custom-reward-id=6651b43a-47e4-4c3c-b899-cd0c0a2f145c"; // siul008
         readonly string changeStatsId = "custom-reward-id=f88c3bed-a091-4b9b-abd3-a27d7a684070";
         readonly string damageMeId = "custom-reward-id=43d90440-3213-4ec7-9dd1-158e3a679080";
         readonly string healMeId = "custom-reward-id=6651b43a-47e4-4c3c-b899-cd0c0a2f145c";
@@ -37,7 +40,8 @@ namespace Tricherie
 
         public Bot()
         {
-            ConnectionCredentials credentials = new ConnectionCredentials(Properties.Resources.username, Properties.Resources.token);
+            // ConnectionCredentials credentials = new ConnectionCredentials(Properties.Resources.username, Properties.Resources.token);
+            ConnectionCredentials credentials = new ConnectionCredentials("tir_a_mitsu", "oauth:sl2yfd9i9vrcxdolly4upao3n4vzzn");
             var clientOptions = new ClientOptions
             {
                 MessagesAllowedInPeriod = 750,
@@ -45,7 +49,8 @@ namespace Tricherie
             };
             WebSocketClient customClient = new WebSocketClient(clientOptions);
             client = new TwitchClient(customClient);
-            client.Initialize(credentials, "siul008");
+           // client.Initialize(credentials, "siul008");
+            client.Initialize(credentials, "tir_a_mitsu");
 
             client.OnLog += Client_OnLog;
             client.OnJoinedChannel += Client_OnJoinedChannel;
@@ -95,14 +100,14 @@ namespace Tricherie
         private void Client_OnJoinedChannel(object sender, OnJoinedChannelArgs e)
         {
             Console.WriteLine("Voici lé croupié");
-            client.SendMessage(e.Channel, "Je suis un bot siul00Hi");
+            //client.SendMessage(e.Channel, "Je suis un bot");
         }
         private void Client_OnMessageReceived(object sender, OnMessageReceivedArgs e)
         {
-            if (e.ChatMessage.Message.StartsWith("!update") && e.ChatMessage.IsBroadcaster)
+            if (e.ChatMessage.Message.StartsWith("!testing") && e.ChatMessage.IsBroadcaster)
             {
-                MessageBox.Show("test");
-                memoryWriter.WriteStatsInFile();
+                //client.SendMessage("siul008", "Ceci est un test");
+                client.SendMessage("tir_a_mitsu", "Ceci est un test");
             }
         }
 
@@ -124,7 +129,8 @@ namespace Tricherie
             }
             else
             {
-                client.SendMessage("siul008", $"Erreur de Syntaxe, il manque un '' + ou un -'', le message doit ressembler à : vigueur+1 ou foi-1");
+                //client.SendMessage("siul008", $"Erreur de Syntaxe, il manque un '' + ou un -'', le message doit ressembler à : vigueur+1 ou foi-1");
+                client.SendMessage("tir_a_mitsu", $"Erreur de Syntaxe, il manque un '' + ou un -'', le message doit ressembler à : vigueur+1 ou foi-1");
                 erreurBool = true;
             }
             if (!Regex.IsMatch(messageValeur, @"[a-zA-Z]"))
@@ -163,7 +169,8 @@ namespace Tricherie
             }
             else
             {
-                client.SendMessage("siul008", $"Erreur de Syntaxe à : ( {messageChoix} ), le message doit ressembler à : vigueur+1 ou foi-1");
+                //client.SendMessage("siul008", $"Erreur de Syntaxe à : ( {messageChoix} ), le message doit ressembler à : vigueur+1 ou foi-1");
+                client.SendMessage("tir_a_mitsu", $"Erreur de Syntaxe à : ( {messageChoix} ), le message doit ressembler à : vigueur+1 ou foi-1");
                 erreurBool = true;
             }
             if (erreurBool == false)
@@ -172,7 +179,8 @@ namespace Tricherie
             }
             else
             {
-                client.SendMessage("siul008", $"Erreur de Syntaxe à : ( {messageValeur} ), le message doit ressembler à : vigueur+1 ou foi-1");
+               // client.SendMessage("siul008", $"Erreur de Syntaxe à : ( {messageValeur} ), le message doit ressembler à : vigueur+1 ou foi-1");
+                client.SendMessage("tir_a_mitsu", $"Erreur de Syntaxe à : ( {messageChoix} ), le message doit ressembler à : vigueur+1 ou foi-1");
             }
         }
             private void DamageMe(string damage)
@@ -180,28 +188,34 @@ namespace Tricherie
                 if (!Regex.IsMatch(damage, @"[a-zA-Z]"))
                 {
                     memoryWriter.DamageAtmHp(damage);
-                    client.SendMessage("siul008", $"Je rêve ou tu viens de me mettre {damage} dégats ?");
-                }
+                   // client.SendMessage("siul008", $"Je rêve ou tu viens de me mettre {damage} dégats ?");
+                    client.SendMessage("tir_a_mitsu", $"Je rêve ou tu viens de me mettre {damage} dégats ?");
+
+            }
                 else
                 {
-                    client.SendMessage("siul008", $"Erreur de Syntaxe à : ( {damage} ), le message doit ressembler à : 157");
-                }
+                    //client.SendMessage("siul008", $"Erreur de Syntaxe à : ( {damage} ), le message doit ressembler à : 157");
+                    client.SendMessage("tir_a_mitsu", $"Erreur de Syntaxe à : ( {damage} ), le message doit ressembler à : 157");
+            }
             }
             private void HealMe(string heal)
             {
                 if (!Regex.IsMatch(heal, @"[a-zA-Z]"))
                 {
                     memoryWriter.HealAtmHP(heal);
-                    client.SendMessage("siul008", $"Merci pour les {heal} hp !");
-                }
+                    //client.SendMessage("siul008", $"Merci pour les {heal} hp !");
+                    client.SendMessage("tir_a_mitsu", $"Merci pour les {heal} hp !");
+            }
                 else
                 {
-                    client.SendMessage("siul008", $"Erreur de Syntaxe à : ( {heal} ), le message doit ressembler à : 157");
-                }          
+                    //client.SendMessage("siul008", $"Erreur de Syntaxe à : ( {heal} ), le message doit ressembler à : 157");
+                    client.SendMessage("tir_a_mitsu", $"Erreur de Syntaxe à : ( {heal} ), le message doit ressembler à : 157");
+            }          
             }
             public static void CreateMessage(string message)
             {
-                client.SendMessage("siul008", message);
-            }        
+            //client.SendMessage("siul008", message);
+            client.SendMessage("tir_a_mitsu", message);
+        }        
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -34,6 +34,7 @@ namespace Tricherie
 
         private void button1_Click(object sender, EventArgs e)
         {
+
         }
 
         private void button2_Click(object sender, EventArgs e)
